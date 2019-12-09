4c2f6d2c18119132fd6fe5919a0b83d44ace59e9
edoardogallinelli <37400390+edoardogallinelli@users.noreply.github.com>
2019-12-09 11:02:28 +0100

    Nav bar

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 var path = require('path');
 var apos = require('apostrophe')({
   shortName: 'ikea',
-
   bundles: ['apostrophe-events'], //boudles degli eventi
   modules: {
     'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
     'articoli': {},
     'articoli-pages': { extend: 'apostrophe-pieces-pages' },
-    'articoli-widgets': {},
+    'articoli-widgets': {extend: 'apostrophe-pieces-widgets'},
     'salotti': {},
     'salotti-pages': { extend: 'apostrophe-pieces-pages' },
-   
+    'camere': {},
+    'camere-pages': {extend: 'apostrophe-pieces-pages'},
+    'camere-widgets': {extend: 'apostrophe-pieces-widgets'},
     'apostrophe-workflow': {
       alias: 'workflow',
       replicateAcrossLocales: false
     },
     'apostrophe-events': {},
-    'apostrophe-events-pages': {
-      extend: 'apostrophe-pieces-pages',
-    },
-    'apostrophe-events-widgets': {},
+    'apostrophe-events-pages': {extend: 'apostrophe-pieces-pages'},
+    'apostrophe-events-widgets': {extend: 'apostrophe-pieces-widgets'},
     'apostrophe-search': {},
     'apostrophe-pages': {
       park: [
@@ -42,6 +41,13 @@ var apos = require('apostrophe')({
           slug: '/lista-articoli',
           type: 'articoli-pages',
           label: 'Articoli',
+          published: true
+        },
+        {
+          title: 'Camere',
+          slug: '/lista-camere',
+          type: 'camere-pages',
+          label: 'Camere',
           published: true
         },
         {
